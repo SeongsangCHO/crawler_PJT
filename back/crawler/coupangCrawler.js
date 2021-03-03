4a7ece078967673bac7c7ff023c1923d631eb604
seongsangcho <liasqui31@google.com>
2021-03-03 16:29:44 +0900

    refactor : 쿠팡 함수 유틸파일로 분할
    관련함수 유틸로 분할

--- back/crawler/coupangCrawler.js
+++ back/crawler/coupangCrawler.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const pageSet = require("./pageSetting");
+const coupangUtils = require("./coupangUtils");
 let db = require("../config/db_config");
 
 const CRAWL_PAGES = 1;
@@ -8,50 +9,98 @@ const SUCCESS = 1;
 const FAILURE = 0;
 const LIST_SIZE = 72;
 
-//페이지당 광고 삭제
-const removeAd = async (page) => {
-  for (let i = 0; i < LIST_SIZE; i++) {
-    await page.evaluate(() => {
-      let badge = document.querySelector(".search-product__ad-badge");
-      if (badge) badge.remove();
-    });
-  }
-};
+// const getProductData = async (page, productAmountPerPage) => {
+//   let productData = [];
+//   let priority = 1;
 
-//전체 상품갯수, 전체 페이지 숫자 구하기
-const getSearchRange = async (page) => {
-  let totalProducts = 0;
-  let lastPageNumber = 0;
-  try {
-    totalProducts = await page.$eval(
-      `
-    #searchOptionForm > div.search-header strong`,
-      (element) => {
-        return element.innerText
-          .slice(1, element.innerText.length - 1)
-          .replace(/,/gi, "");
-      }
-    );
+//   for (let idx = 1; idx <= productAmountPerPage; idx++) {
+//     let productObj = {};
+//       try {
+//         //우선순위, 제목, 가격, link 를 찾아서 추가해주는 part, TODO : unit-price도 추가해야할지 고민
+//         productObj["priority"] = priority++;
+//         productObj["title"] = await page.$eval(
+//           `#productList li:nth-child(${idx}) div.name`,
+//           (element) => {
+//             return element.textContent || "";
+//           }
+//         );
+//         productObj["price"] = await page.$eval(
+//           `#productList li:nth-child(${idx}) .price-value`,
+//           (element) => {
+//             return element.textContent || "";
+//           }
+//         );
+//         productObj["link"] = await page.$eval(
+//           `#productList li:nth-child(${idx}) a`,
+//           (element) => {
+//             return element.href || "";
+//           }
+//         );
+//         productObj["imgsrc"] = await page.$eval(
+//           `#productList li:nth-child(${idx}) img`,
+//           (element) => {
+//             return element.getAttribute("src");
+//           }
+//         );
+//       } catch (error) {
+//         console.error(error);
+//       }
+//       //데이터가 존재할때만 추가함.
+//       if (productObj.title && productObj.price && productObj.link)
+//         productData.push(productObj);
+//       //존재하지않으면 우선순위 증가하지 않도록 --
+//       else {
+//         priority--;
+//         continue;
+//       }
+//     }
+//     return productData;
+// }
 
-    lastPageNumber = Math.ceil(totalProducts / LIST_SIZE);
-    if (+totalProducts === 0) {
-      //검색결과가 없음
-      console.log("검색결과가 없습니다.");
-      await page.close(); // 페이지 닫기
-      await browser.close(); // 브라우저 닫기
-      return {
-        totalProducts: NO_SEARCH_DATA,
-        lastPageNumber: NO_SEARCH_DATA,
-      };
-    }
-  } catch (error) {
-    console.error(error);
-  }
-  return {
-    totalProducts: totalProducts,
-    lastPageNumber: lastPageNumber,
-  };
-};
+// //페이지당 광고 삭제
+// const removeAd = async (page) => {
+//   for (let i = 0; i < LIST_SIZE; i++) {
+//     await page.evaluate(() => {
+//       let badge = document.querySelector(".search-product__ad-badge");
+//       if (badge) badge.remove();
+//     });
+//   }
+// };
+
+// //전체 상품갯수, 전체 페이지 숫자 구하기
+// const getSearchRange = async (page) => {
+//   let totalProducts = 0;
+//   let lastPageNumber = 0;
+//   try {
+//     totalProducts = await page.$eval(
+//       `
+//     #searchOptionForm > div.search-header strong`,
+//       (element) => {
+//         return element.innerText
+//           .slice(1, element.innerText.length - 1)
+//           .replace(/,/gi, "");
+//       }
+//     );
+
+//     lastPageNumber = Math.ceil(totalProducts / LIST_SIZE);
+//     if (+totalProducts === 0) {
+//       //검색결과가 없음
+//       console.log("검색결과가 없습니다.");
+//       await page.close(); // 페이지 닫기
+//       await browser.close(); // 브라우저 닫기
+//       return {
+//         totalProducts: NO_SEARCH_DATA,
+//         lastPageNumber: NO_SEARCH_DATA,
+//       };
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   return {
+//     totalProducts: totalProducts,
+//     lastPageNumber: lastPageNumber,
+//   };
+// };
 
 const coupangCrawler = async (searchText, linkId) => {
   let start = await new Date().getTime();
@@ -74,7 +123,7 @@ const coupangCrawler = async (searchText, linkId) => {
   );
 
   // let lastPageNumber;
-  const searchRange = await getSearchRange(page);
+  const searchRange = await coupangUtils.getSearchRange(page);
   let totalProducts = searchRange.totalProducts;
   let lastPageNumber = searchRange.lastPageNumber;
 
@@ -91,7 +140,6 @@ const coupangCrawler = async (searchText, linkId) => {
   let productData = [];
 
   try {
-    let priority = 1;
     //1 ~ 3페이지까지 크롤링
     for (let pageNumber = 1; pageNumber <= CRAWL_PAGES; pageNumber++) {
       if (pageNumber != 1) {
@@ -102,53 +150,12 @@ const coupangCrawler = async (searchText, linkId) => {
         );
       }
 
-      await removeAd(page);
+      await coupangUtils.removeAd(page);
       //페이지당 상품 갯수 => 페이지당 광고를 삭제하고나서 해당 페이지에 존재하는 상품갯수
       let productAmountPerPage = await page.evaluate(() => {
         return document.querySelectorAll(`#productList li`).length;
       });
-
-      for (let idx = 1; idx <= productAmountPerPage; idx++) {
-        let productObj = {};
-        try {
-          //우선순위, 제목, 가격, link 를 찾아서 추가해주는 part, TODO : unit-price도 추가해야할지 고민
-          productObj["priority"] = priority++;
-          productObj["title"] = await page.$eval(
-            `#productList li:nth-child(${idx}) div.name`,
-            (element) => {
-              return element.textContent || "";
-            }
-          );
-          productObj["price"] = await page.$eval(
-            `#productList li:nth-child(${idx}) .price-value`,
-            (element) => {
-              return element.textContent || "";
-            }
-          );
-          productObj["link"] = await page.$eval(
-            `#productList li:nth-child(${idx}) a`,
-            (element) => {
-              return element.href || "";
-            }
-          );
-          productObj["imgsrc"] = await page.$eval(
-            `#productList li:nth-child(${idx}) img`,
-            (element) => {
-              return element.getAttribute("src");
-            }
-          );
-        } catch (error) {
-          console.error(error);
-        }
-        //데이터가 존재할때만 추가함.
-        if (productObj.title && productObj.price && productObj.link)
-          productData.push(productObj);
-        //존재하지않으면 우선순위 증가하지 않도록 --
-        else {
-          priority--;
-          continue;
-        }
-      }
+      productData = await coupangUtils.getProductData(page, productAmountPerPage);
     }
   } catch (error) {
     console.error(error);
@@ -157,14 +164,12 @@ const coupangCrawler = async (searchText, linkId) => {
   await page.close(); // 페이지 닫기
   await browser.close(); // 브라우저 닫기
 
-  let end = await new Date().getTime();
-  console.log("쿠팡크롤러 time :" + (end - start) / 1000);
+  console.log("쿠팡크롤러 time :" + (await new Date().getTime() - start) / 1000);
 
   if (dataInsert(productData, linkId) == FAILURE) {
     return FAILURE;
   }
   return SUCCESS;
-  //crawling part end //
 };
 
 function dataInsert(crawlerData, linkId) {
---
+++ back/crawler/coupangUtils.js
@@ -0,0 +1,98 @@
+const LIST_SIZE = 72;
+
+const getProductData = async (page, productAmountPerPage) => {
+  let productData = [];
+  let priority = 1;
+
+  for (let idx = 1; idx <= productAmountPerPage; idx++) {
+    let productObj = {};
+      try {
+        //우선순위, 제목, 가격, link 를 찾아서 추가해주는 part, TODO : unit-price도 추가해야할지 고민
+        productObj["priority"] = priority++;
+        productObj["title"] = await page.$eval(
+          `#productList li:nth-child(${idx}) div.name`,
+          (element) => {
+            return element.textContent || "";
+          }
+        );
+        productObj["price"] = await page.$eval(
+          `#productList li:nth-child(${idx}) .price-value`,
+          (element) => {
+            return element.textContent || "";
+          }
+        );
+        productObj["link"] = await page.$eval(
+          `#productList li:nth-child(${idx}) a`,
+          (element) => {
+            return element.href || "";
+          }
+        );
+        productObj["imgsrc"] = await page.$eval(
+          `#productList li:nth-child(${idx}) img`,
+          (element) => {
+            return element.getAttribute("src");
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+      //데이터가 존재할때만 추가함.
+      if (productObj.title && productObj.price && productObj.link)
+        productData.push(productObj);
+      //존재하지않으면 우선순위 증가하지 않도록 --
+      else {
+        priority--;
+        continue;
+      }
+    }
+    return productData;
+}
+
+//페이지당 광고 삭제
+const removeAd = async (page) => {
+  for (let i = 0; i < LIST_SIZE; i++) {
+    await page.evaluate(() => {
+      let badge = document.querySelector(".search-product__ad-badge");
+      if (badge) badge.remove();
+    });
+  }
+};
+
+//전체 상품갯수, 전체 페이지 숫자 구하기
+const getSearchRange = async (page) => {
+  let totalProducts = 0;
+  let lastPageNumber = 0;
+  try {
+    totalProducts = await page.$eval(
+      `
+    #searchOptionForm > div.search-header strong`,
+      (element) => {
+        return element.innerText
+          .slice(1, element.innerText.length - 1)
+          .replace(/,/gi, "");
+      }
+    );
+
+    lastPageNumber = Math.ceil(totalProducts / LIST_SIZE);
+    if (+totalProducts === 0) {
+      //검색결과가 없음
+      console.log("검색결과가 없습니다.");
+      await page.close(); // 페이지 닫기
+      await browser.close(); // 브라우저 닫기
+      return {
+        totalProducts: NO_SEARCH_DATA,
+        lastPageNumber: NO_SEARCH_DATA,
+      };
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  return {
+    totalProducts: totalProducts,
+    lastPageNumber: lastPageNumber,
+  };
+};
+
+exports.getProductData = getProductData;
+exports.removeAd = removeAd;
+exports.getSearchRange = getSearchRange;
