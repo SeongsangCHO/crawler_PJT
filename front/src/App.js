4be70cdc6dc97d403031804ad45b248eb6492244
Seongsangcho <liasqui31@gmail.com>
2021-04-22 23:03:51 +0900

    fix: 파일명 변경

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,8 @@
-import React, { useEffect,useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Register from "./pages/Register";
-import styled from 'styled-components'
-import Register2 from "./pages/Register2";
+
 import Home from "./pages/Home";
 import MyLink from "./pages/MyLink";
 import NotificationContainer from "react-notifications/lib/NotificationContainer";
@@ -14,12 +11,11 @@ function App() {
   const headerRef = useRef(null);
   return (
     <Router>
-      <Header ref={headerRef}/>
+      <Header ref={headerRef} />
       <NotificationContainer />
       <Switch>
         <Route exact path={"/"} component={Home} />
-        <Route exact path={"/mylink"} component={MyLink}/>
-        <Route exact path={"/register"} component={Register2} />
+        <Route exact path={"/mylink"} component={MyLink} />
       </Switch>
     </Router>
   );
